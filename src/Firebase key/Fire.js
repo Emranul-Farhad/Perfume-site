569141e538c82516f020ed5151a13320ea643dd2
unknown <emranulfarhad@gmail.com>
2022-04-28 15:16:51 +0600

    first commit

---
+++ src/Firebase key/Fire.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCb6Xy4ipX57RNpFiSgOJKjYq_rs3V1FgU",
+  authDomain: "doctor-porta-74bcf.firebaseapp.com",
+  projectId: "doctor-porta-74bcf",
+  storageBucket: "doctor-porta-74bcf.appspot.com",
+  messagingSenderId: "525730659462",
+  appId: "1:525730659462:web:5bcf249061d431a67c2507"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = (app)
+
+export default auth
