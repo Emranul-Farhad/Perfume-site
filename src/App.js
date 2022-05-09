319ce8baab28cf43bc3f1a1ba371899040ed4b85
unknown <emranulfarhad@gmail.com>
2022-05-09 15:53:15 +0600

    require page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Home from './Components/HomePage/Home/Home';
 import Login from './Components/Login/Login';
 import Manages from './Components/Manages/Manages';
 import Footer from './Footerpage/Footer';
+import Require from './Require/Require';
+import MyProducts from './Components/My products/MyProducts';
 
 
 function App() {
@@ -17,8 +19,9 @@ function App() {
 
       <Routes>
         <Route path='/' element={<Home></Home>} ></Route>
-        <Route path='/Add-products' element={<Addproducts></Addproducts>} ></Route>
-        <Route path='/Manage' element={<Manages></Manages>} ></Route>
+        <Route path='/Add-products' element={ <Require> <Addproducts></Addproducts> </Require> } ></Route>
+        <Route path='/Manage' element={ <Require> <Manages></Manages> </Require> } ></Route>
+        <Route path='/My-products' element={ <Require> <MyProducts></MyProducts> </Require> } ></Route>
         <Route path='/login' element={<Login></Login>} ></Route>
       </Routes>
       <Footer></Footer>
---
+++ src/Components/My products/MyProducts.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyProducts = () => {
+    return (
+        <div className='pt-20'>
+            <p>fff</p>
+        </div>
+    );
+};
+
+export default MyProducts;
